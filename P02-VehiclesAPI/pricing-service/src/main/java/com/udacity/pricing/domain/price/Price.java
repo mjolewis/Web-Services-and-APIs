5ac24e2c1388ff1f97079ba4c9aed7e5d533a1de
Michael Lewis <mjolewis@gmail.com>
2021-06-01 22:05:26 -0400

    FEAT: Convert to microservice archiecture

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,12 +1,17 @@
 package com.udacity.pricing.domain.price;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 /**
  * Represents the price of a given vehicle, including currency.
  */
+@Entity
 public class Price {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String currency;
     private BigDecimal price;
     private Long vehicleId;
@@ -14,10 +19,25 @@ public class Price {
     public Price() {
     }
 
+    public Price(Long id, String currency, BigDecimal price, Long vehicleId) {
+        this.id = id;
+        this.currency = currency;
+        this.price = price;
+        this.vehicleId = vehicleId;
+    }
+
     public Price(String currency, BigDecimal price, Long vehicleId) {
         this.currency = currency;
         this.price = price;
         this.vehicleId = vehicleId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getCurrency() {
